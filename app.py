40f0bebf0249303135e1889d452d844497ee4334
Sarmad Ali <sarmad.ali77771@gmail.com>
2025-08-20 22:15:29 +0500

    Update app.py
    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ skills = [
     {"name": "Bash", "description": "Bash is a Unix shell and command language for automation"},
     {"name": "Kubernetes", "description": "Kubernetes is an open-source container orchestration tool"},
     {"name": "Ansible", "description": "Ansible is an open-source automation tool.."},
-    {"name": "Jenkins", "description": "Jenkins is an open-source automation server for CI/CD"},
+    {"name": "Jenkins", "description": "Jenkins is an open-source automation server for CI/CD..."},
     {"name": "Terraform", "description": "Terraform is an infrastructure as code tool..."},
     {"name": "Grafana", "description": "Grafana is an open-source analytics and monitoring..."},
     {"name": "Prometheus", "description": "Prometheus is an open-source monitoring system..."},
